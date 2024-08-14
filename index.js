4316e9be33b3e89d006e1039d5ea15a9eb7f1a7e
PATEL96 <patel.raj.official.96@gmail.com>
2024-08-14 19:05:34 +0530

    feat: options Update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { execSync } = require('child_process');
 const gitClone = require('git-clone');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 const repoUrl = 'https://github.com/PATEL96/BaseTemplate-Rainbowkit.git';
 
@@ -19,8 +20,28 @@ gitClone(repoUrl, targetPath, null, () => {
     console.log(`Cloned to ${targetPath}`);
     fs.rmSync(path.join(targetPath, '.git'), { recursive: true, force: true });
     console.log('Removed .git directory');
-    console.log(`Project ${projectName} is ready!`);
-    console.log(`run tese to move ahead`)
-    console.log(`cd ${projectName}`)
-    console.log('yarn install')
+
+    // Change directory to the target path
+    process.chdir(targetPath);
+
+    // Create a readline interface to get user input
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Which package manager do you want to use? (yarn/npm): ', (answer) => {
+        const packageManager = answer.toLowerCase() === 'npm' ? 'npm' : 'yarn';
+        
+        console.log(`Installing packages with ${packageManager}...`);
+        execSync(`${packageManager} install`, { stdio: 'inherit' });
+
+        console.log(`Project ${projectName} is ready!`);
+        console.log(`To start working on your project, run:`);
+        console.log(`cd ${projectName}`);
+        console.log(`${packageManager} run dev`); // or any other command you want to suggest
+
+        // Close the readline interface
+        rl.close();
+    });
 });
