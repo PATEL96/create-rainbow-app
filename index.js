83a5ef549d87054779cdffa169ae87aeeab9aab5
PATEL96 <patel.raj.official.96@gmail.com>
2024-08-14 19:22:37 +0530

    feat: options Update

--- index.js
+++ index.js
@@ -4,40 +4,44 @@ const { execSync } = require('child_process');
 const gitClone = require('git-clone');
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk'); // Import chalk
 
-const repoUrl = 'https://github.com/PATEL96/BaseTemplate-Rainbowkit.git';
+(async () => {
+    // Dynamically import chalk
+    const chalk = (await import('chalk')).default;
 
-// Check if yarn is installed
-try {
-    execSync('yarn --version', { stdio: 'ignore' });
-} catch (error) {
-    console.error(chalk.red('Yarn is required to run this script.'));
-    console.log(chalk.yellow('Please install Yarn from the following link:'));
-    console.log(chalk.blue('https://yarnpkg.com/getting-started/install'));
-    process.exit(1);
-}
+    const repoUrl = 'https://github.com/PATEL96/BaseTemplate-Rainbowkit.git';
 
-const projectName = process.argv[2];
-if (!projectName) {
-    console.error(chalk.red('Please provide a project name.'));
-    process.exit(1);
-}
+    // Check if yarn is installed
+    try {
+        execSync('yarn --version', { stdio: 'ignore' });
+    } catch (error) {
+        console.error(chalk.red('Yarn is required to run this script.'));
+        console.log(chalk.yellow('Please install Yarn from the following link:'));
+        console.log(chalk.blue('https://yarnpkg.com/getting-started/install'));
+        process.exit(1);
+    }
 
-const targetPath = path.join(process.cwd(), projectName);
+    const projectName = process.argv[2];
+    if (!projectName) {
+        console.error(chalk.red('Please provide a project name.'));
+        process.exit(1);
+    }
 
-gitClone(repoUrl, targetPath, null, () => {
-    fs.rmSync(path.join(targetPath, '.git'), { recursive: true, force: true });
+    const targetPath = path.join(process.cwd(), projectName);
 
-    // Change directory to the target path
-    process.chdir(targetPath);
+    gitClone(repoUrl, targetPath, null, () => {
+        fs.rmSync(path.join(targetPath, '.git'), { recursive: true, force: true });
 
-    // Install required packages
-    console.log(chalk.blue('Installing packages...'));
-    execSync('yarn install', { stdio: 'inherit' }); // or 'npm install'
+        // Change directory to the target path
+        process.chdir(targetPath);
 
-    console.log(chalk.green(`Project ${projectName} is ready!`));
-    console.log(chalk.yellow(`To start working on your project, run:`));
-    console.log(chalk.cyan(`cd ${projectName}`));
-    console.log(chalk.magenta('yarn dev')); // or any other command you want to suggest
-});
+        // Install required packages
+        console.log(chalk.blue('Installing packages...'));
+        execSync('yarn install', { stdio: 'inherit' }); // or 'npm install'
+
+        console.log(chalk.green(`Project ${projectName} is ready!`));
+        console.log(chalk.yellow(`To start working on your project, run:`));
+        console.log(chalk.cyan(`cd ${projectName}`));
+        console.log(chalk.magenta('yarn dev')); // or any other command you want to suggest
+    });
+})();
