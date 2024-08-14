37d4e7c2897da2709eb02c147d3e7043468ad6df
PATEL96 <patel.raj.official.96@gmail.com>
2024-08-14 19:20:09 +0530

    feat: options Update

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const { execSync } = require('child_process');
 const gitClone = require('git-clone');
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk'); // Import chalk
 
 const repoUrl = 'https://github.com/PATEL96/BaseTemplate-Rainbowkit.git';
 
+// Check if yarn is installed
+try {
+    execSync('yarn --version', { stdio: 'ignore' });
+} catch (error) {
+    console.error(chalk.red('Yarn is required to run this script.'));
+    console.log(chalk.yellow('Please install Yarn from the following link:'));
+    console.log(chalk.blue('https://yarnpkg.com/getting-started/install'));
+    process.exit(1);
+}
+
 const projectName = process.argv[2];
 if (!projectName) {
-    console.error('Please provide a project name.');
+    console.error(chalk.red('Please provide a project name.'));
     process.exit(1);
 }
 
@@ -22,11 +33,11 @@ gitClone(repoUrl, targetPath, null, () => {
     process.chdir(targetPath);
 
     // Install required packages
-    console.log('Installing packages...');
+    console.log(chalk.blue('Installing packages...'));
     execSync('yarn install', { stdio: 'inherit' }); // or 'npm install'
 
-    console.log(`Project ${projectName} is ready!`);
-    console.log(`To start working on your project, run:`);
-    console.log(`cd ${projectName}`);
-    console.log('yarn dev'); // or any other command you want to suggest
+    console.log(chalk.green(`Project ${projectName} is ready!`));
+    console.log(chalk.yellow(`To start working on your project, run:`));
+    console.log(chalk.cyan(`cd ${projectName}`));
+    console.log(chalk.magenta('yarn dev')); // or any other command you want to suggest
 });
