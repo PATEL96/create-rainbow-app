d2df38a1510356beba395f9a16c72154b83509ef
PATEL96 <contact@patel96.live>
2025-07-12 14:44:19 +0530

    feat: v5.0.0 | 12/07.

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const path = require("path");
 function displayWelcomeBanner(chalk) {
     console.clear();
     console.log(
-        chalk.cyan.bold(`
+        chalk.orange.bold(`
      ██╗    ██╗███████╗██████╗ ██████╗     ██████╗  █████╗ ██████╗ ██████╗
      ██║    ██║██╔════╝██╔══██╗╚════██╗    ██╔══██╗██╔══██╗██╔══██╗██╔══██╗
      ██║ █╗ ██║█████╗  ██████╔╝ █████╔╝    ██║  ██║███████║██████╔╝██████╔╝
